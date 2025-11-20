3aead873fbbd80864093a74792e7fd68be2ee34d
mintycake420 <yotamkatz2000@gmail.com>
2025-11-21 00:18:32 +0200

    Created MVC structure
    Created MVC structure

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import minesweeper.model.*;
+import minesweeper.controller.*;
+import minesweeper.view.*;
+
 public class Main {
     public static void main(String[] args) {
+
+
         System.out.println("Hello world!");
     }
 }
